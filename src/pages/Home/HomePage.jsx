6b0a6b0bdd9ee16e6418d7b2920ff893b6097a3c
jezi89 <13301753+jezi89@users.noreply.github.com>
2025-06-04 03:20:45 +0200

    contiuing search, intermediate save

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -8,6 +8,7 @@
  */
 
 import SearchBar from "@/components/search/Searchbar.jsx";
+import {useSearchPoems} from "@/hooks/useSearchPoems.js";
 
 /**
  * HomePage component
@@ -18,12 +19,21 @@ import SearchBar from "@/components/search/Searchbar.jsx";
 export function HomePage() {
     // Render welcome message and search bar
     // Handle any error states
-    
+    const {
+        results,
+        loading,
+        error,
+        handleSearch,
+        searchMeta
+    } = useSearchPoems();
+
+
     return (
         <div className="page-home">
             <h1 className="page-title">Welkom bij GedichtGevel</h1>
             <p className="page-subtitle">Ontdek de wereld van poëzie</p>
             {/* <SearchBar /> */}
+            <SearchBar onSearch={handleSearch}/>
         </div>
     );
 }
