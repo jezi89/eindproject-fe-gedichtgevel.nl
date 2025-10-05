26e82504a966de134e7245e80b63f458271bdc43
jezi89 <13301753+jezi89@users.noreply.github.com>
2025-10-05 22:17:20 +0200

    refactor eraMapping.js: streamline author year calculations and improve function naming

--- src/components/Core/Recording/PijltjeIcon.jsx
+++
@@ -1,4 +0,0 @@
-export const PijltjeIcon = () => (<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1 1L8 10L15 1" stroke="#F9F4ED" strokeWidth="2.5"/>
-</svg>);
-
