1235454358bf56618c8e97fc5bdf52662e90555b
jezi89 <13301753+jezi89@users.noreply.github.com>
2025-10-05 23:43:11 +0200

    FINAL cleanupfixes

---
+++ src/components/Core/Recording/FilterPanel.jsx
@@ -0,0 +1,15 @@
+/*
+import {PijltjeIcon} from './PijltjeIcon';
+import styles from './RecordingPage.module.scss';
+
+export function FilterPanel() {
+    return (
+        <div className={styles.FilterPanel}>
+            <div><span>Taal</span> <PijltjeIcon/></div>
+            <div><span>Lengte</span> <PijltjeIcon/></div>
+            <div><span>Tijdperk</span> <PijltjeIcon/></div>
+            <div><span>Genre</span> <PijltjeIcon/></div>
+        </div>
+    );
+}
+*/
